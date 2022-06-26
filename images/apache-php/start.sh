cee1f5ae368b6f89c77e343b54b3020e835ec785
jurapro <jurapro@yandex.ru>
2022-06-26 20:50:43 +0700

    update structure directories

--- images/apache-php/start.sh
+++ images/apache-php/start.sh
@@ -2,7 +2,7 @@
 
 
 echo "starting services ..."
-
+chmod 777 -R /var/www/html
 service ssh start
 apache2-foreground
 
--- images/django/start.sh
+++ images/django/start.sh
@@ -4,7 +4,6 @@ echo "Starting django"
 if [ -f /web_django/manage.py ]; then echo "Directory not empty" 
 else
 django-admin startproject web_django /web_django;
-fi
+touch /web_django/db.sqlite3
 chmod 777 -R /web_django
-
-service ssh start
+fi
