391645e74aa9c971923cdc6a7a370d31afc8188c
jurapro <jurapro@yandex.ru>
2022-07-13 19:03:06 +0700

    added port generation to user add script

--- scripts/adduser.sh
+++ scripts/adduser.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd )"
+APP_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd )"
 
-login="usr_$(openssl rand -base64 3)"
+login="user_$(openssl rand -base64 3)"
 password=$(openssl rand -base64 3)
 template="wsr"
+
+function getRandomPort {
+   value=0
+   while [ "$value" -le $1 ]
+   do
+   value=$(( $RANDOM * 2 ))
+   done
+
+   echo $(( $value ))
+}
+
+min_port=10000
+web_port=$( getRandomPort $min_port)
 
 for ARGUMENT in "$@"
 do
@@ -17,34 +30,49 @@ do
 done
 
 
-if [ -d $SCRIPT_DIR/users/$login ]
+if [ -d $APP_DIR/users/$login ]
 then
-echo "The '$SCRIPT_DIR/users/$login' directory exists! Choose a different username!" >&2
+echo "The '$APP_DIR/users/$login' directory exists! Choose a different username!" >&2
 exit 1
 fi
 
-if [ ! -d $SCRIPT_DIR/templates/$template ]; then
-echo "Template '$SCRIPT_DIR/templates/$template' not found!" >&2
-echo "Choose from the following templates: "$(ls $SCRIPT_DIR/templates/) >&2
+if [ ! -d $APP_DIR/templates/$template ]; then
+echo "Template '$APP_DIR/templates/$template' not found!" >&2
+echo "Choose from the following templates: "$(ls $APP_DIR/templates/) >&2
 exit 1
 fi
 
 echo "Create a user: $login, password: $password, template: $template"
 
-echo "Copying the template '$SCRIPT_DIR/templates/$template'..."
-cp $SCRIPT_DIR/templates/$template -a -T $SCRIPT_DIR/users/$login
+echo "Copying the template '$APP_DIR/templates/$template'..."
+cp $APP_DIR/templates/$template -a -T $APP_DIR/users/$login
 echo "Copy completed"
 echo "Applying user setting..."
 
 
-if [ ! -f $SCRIPT_DIR/users/$login/.env ]; then
-echo "The '$SCRIPT_DIR/users/$login/.env' file not exists" >&2
+if [ ! -f $APP_DIR/users/$login/.env ]; then
+echo "The '$APP_DIR/users/$login/.env' file not exists" >&2
 exit 1
 fi
 
-sed -i -r "s/^(USER_LOGIN=).*/\1${login}/" $SCRIPT_DIR/users/$login/.env
-sed -i -r "s/^(USER_PASSWORD=).*/\1${password}/" $SCRIPT_DIR/users/$login/.env
-sed -i -r "s/^(DB_NAME=).*/\1db_${login}/" $SCRIPT_DIR/users/$login/.env
+sed -i -r "s/^(USER_LOGIN=).*/\1${login}/" $APP_DIR/users/$login/.env
+sed -i -r "s/^(USER_PASSWORD=).*/\1${password}/" $APP_DIR/users/$login/.env
+sed -i -r "s/^(DB_NAME=).*/\1db_${login}/" $APP_DIR/users/$login/.env
+sed -i -r "s/^(USER_DB_PASSWORD=).*/\1${password}/" $APP_DIR/users/$login/.env
+sed -i -r "s/^(WEB_PORT=).*/\1${web_port}/" $APP_DIR/users/$login/.env
+
+for ARGUMENT in $(grep SSH_PORT_* $APP_DIR/users/$login/.env)
+do
+   port=$( getRandomPort $min_port)
+
+   while [[ $(grep $port $APP_DIR/users/*/.env) ]]
+   do
+   port=$( getRandomPort $min_port)
+   done 
+
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+   sed -i -r "s/^($KEY=).*/\1${port}/" $APP_DIR/users/$login/.env
+done
 
 
 echo "User settings applied"
