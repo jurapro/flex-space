11a3dd88a3f130f6bcbdec0525fce5796563101c
jurapro <jurapro@yandex.ru>
2022-08-05 00:39:59 +0700

    Refactoring template and adding deluser.sh

---
+++ scripts/deluser.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+APP_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd )"
+
+if [[ $1 = "all" ]]; then
+   for USER in $(ls $APP_DIR/users/)
+   do
+      if [[ -e $APP_DIR/users/$USER/docker-compose.yml  ]]; then
+         echo ""
+         echo "Deleting user '$USER'"
+         docker-compose -f $APP_DIR/users/$USER/docker-compose.yml down && rm -rf $APP_DIR/users/$USER
+      fi
+   done
+   exit 0
+fi
+
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+if [ -z ${user+x} ]; then
+echo "User not selected!" >&2
+echo "Choose from the following users: "$(ls $APP_DIR/users/) >&2
+exit 1
+fi
+
+if [ ! -d $APP_DIR/users/$user ]; then
+echo "User '$APP_DIR/users/$user' not found!" >&2
+echo "Choose from the following users: "$(ls $APP_DIR/users/) >&2
+exit 1
+fi
+
+echo "Deleting user '$user'"
+docker-compose -f $APP_DIR/users/$user/docker-compose.yml down && rm -rf $APP_DIR/users/$user
